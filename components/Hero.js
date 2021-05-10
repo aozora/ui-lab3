e20e3e73b4ac635cd96025b2429fca6e1a74c41e
aozora <marcellop@protonmail.com>
2021-05-10 17:05:50 +0200

    feat: wip building hero, animation

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,43 @@
+import gsap from 'gsap';
+import { useEffect, useMemo, useRef } from 'react';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
+  const titleRef = useRef();
+  const pictureRef = useRef();
+  const isClient = typeof window !== 'undefined';
+
+  const timeline = useMemo(() => {
+    return gsap.timeline({ paused: true });
+  }, []);
+
+  useEffect(() => {
+    if (isClient && pictureRef.current) {
+      timeline.fromTo(
+        pictureRef.current,
+        {
+          scale: 1.2
+        },
+        {
+          duration: 2.5,
+          scale: 1,
+          ease: 'power1.out'
+        }
+      );
+      timeline.to(pictureRef.current, {
+        delay: 0.5,
+        duration: 2,
+        y: '-50%',
+        ease: 'power1.out'
+      });
+      timeline.play();
+    }
+  }, [isClient, pictureRef, timeline]);
+
   return (
     <article className={styles.Hero}>
       <div className={styles.HeroTitle}>
-        <h1>
+        <h1 ref={titleRef}>
           Gantan<span className={styles.HeroTitleAsterisk}>*</span>
         </h1>
         <span className={styles.HeroSubTitle}>* lorem ipsum</span>
@@ -26,6 +59,7 @@ const Hero = () => {
       </footer>
       <figure className={styles.HeroPictureContainer}>
         <img
+          ref={pictureRef}
           className={styles.HeroPicture}
           sizes="(max-width: 1900px) 100vw, 1900px"
           srcSet="
